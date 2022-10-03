783e67de02b3577df42a51d132dbdb705cbb5b82
Liu, Minsheng <lambda@liu.ms>
2022-10-04 00:17:20 +0800

    Support collapsing lists.

--- Library/Sources/App/DocumentEditorView.swift
+++ Library/Sources/App/DocumentEditorView.swift
@@ -15,7 +15,9 @@ public struct DocumentEditorView: View {
         } action: { (nodeID, action) in
             .node(nodeID, action)
         }
-        LazyVStack {
+        // We have to use VStack here since LazyVStack will give odd results
+        // when the list updates. This is not a scalable solution.
+        VStack {
             ForEachStore(childStores) { store in
                 NodeEditorView(store: store, documentStore: self.store)
             }
